f34e092f7b03a110cd46e7d2fae437af346c2abf
Rater7tara <nowshinakteremu005@gmail.com>
2024-03-22 21:44:56 +0600

    data fetched and loading added

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,47 +1,49 @@
 import React from 'react';
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useState, Component } from 'react';
 import Loading from '../../../components/Loading/Loading';
 import './Banner.css';
 import { Link } from 'react-router-dom';
-
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 
 const Banner = () => {
-    const [query, setQuery] = useState("");
+    const [query, setQuery] = useState("beautiful");
     const [loading, setLoading] = useState(true);
     const [data, setData] = useState([]);
     const [photos, setPhotos] = useState([]);
-    const [slide, setSlide] = useState([]);
 
     const getPhotos = async (type) => {
-
         let url = `https://api.pexels.com/v1/search?query=${query}`
         if (data?.next_page && type === "next") {
             url = data.next_page;
         }
-
         if (data.prev_page && type === "back") {
             url = data.prev_page;
         }
         setLoading(true);
-        await fetch(url,
-            {
+        try {
+            const response = await fetch(url, {
                 headers: {
-                    Authorization: "R240G2wblUJchdIAlxAcpnqoJANX9h3SorOncFoqhQWlmwMMO03LVSEO",
+                    Authorization: import.meta.env.VITE_APP_YOUR_API_KEY,
                 },
-            })
-            .then((resp) => { return resp.json() })
-            .then((res) => {
-                setLoading(false);
-                setData(res);
-
-                setPhotos(res.photos)
             });
+            const res = await response.json();
+            setLoading(false);
+            setData(res);
+            setPhotos(res.photos);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+            setLoading(false);
+        }
     };
 
     useEffect(() => {
         getPhotos();
-    }, []);
+    }, [query]);
 
     const onKeyDownHandeler = (e) => {
         if (e.keyCode === 13) {
@@ -50,49 +52,102 @@ const Banner = () => {
     };
 
 
-
     return (
         <>
             <div className='main'>
+                {/* Banner section start */}
                 <div className='banner-main'>
                     <div className='banner-text'>
                         <div className='center-text'>
-                            <h1 className='text-white text-5xl ms-5'>Your local source of high quality images videos directly from their creators</h1>
+                            <h1 className='text-white text-5xl ms-5'>Your local source of high quality images & videos directly from their creators</h1>
                             <label className="input input-bordered flex items-center gap-2 my-5">
                                 <input type="text" className="grow inputSearch text-2xl" placeholder="Search photos and videos"
                                     onChange={(e) => setQuery(e.target.value)}
                                     onKeyDown={onKeyDownHandeler}
-                                    value={query}
-
                                 />
 
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-6 text-2xl opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
-
                             </label>
-
                             <p className='text-white ms-5'>Recommended: summer, food, covid-19, real estate, love</p>
                         </div>
-
                     </div>
                 </div>
-                {loading && <Loading></Loading>}
-                <div className='All-img'>
-                    <h1 className='text-2xl font-bold text-start ms-12 mt-2'>Top categories</h1>
-                    <div className="cate-nav navbar-start hidden lg:flex justify-between ms-12">
+                {/* Banner section end */}
+
+                <div className='All-img rounded-lg'>
+                    <h1 className='text-2xl font-bold text-start ms-12 mt-2 mb-4'>Top categories</h1>
+                    {/* Top category start */}
+                    <div className="banner-container">
+                        <Swiper
+                            slidesPerView={5}
+                            spaceBetween={20}
+                            pagination={{ clickable: true }}
+                            modules={[Pagination]}
+                            breakpoints={{
+                                1024: { slidesPerView: 5 },
+                                768: { slidesPerView: 3 },
+                                640: { slidesPerView: 3 },
+                                575: { slidesPerView: 3 }
+                            }}
+                            className="mySwiper m-auto"
+                        >
+                            <SwiperSlide>
+                                <div className='car-in-div relative max-w-xs overflow-hidden bg-cover bg-no-repeat' >
+                                    <img className='max-w-xs transition duration-300 ease-in-out hover:scale-110' src="https://images.pexels.com/photos/46710/pexels-photo-46710.jpeg" alt="" />
+                                    <button onClick={() => setQuery("Summer")} className='text-center-car text-white font-bold text-2xl'>Summer</button>
+                                </div>
+
+                            </SwiperSlide>
+                            <SwiperSlide><div className='car-in-div relative max-w-xs overflow-hidden bg-cover bg-no-repeat'>
+                                <img className='max-w-xs transition duration-300 ease-in-out hover:scale-110' src="https://images.pexels.com/photos/416160/pexels-photo-416160.jpeg" alt="" />
+                                <button onClick={() => setQuery("Cat")} className='text-center-car text-white text-2xl font-bold'>Cat</button>
+                            </div></SwiperSlide>
+                            <SwiperSlide><div className='car-in-div relative max-w-xs overflow-hidden bg-cover bg-no-repeat'>
+                                <img className='max-w-xs transition duration-300 ease-in-out hover:scale-110' src="https://images.pexels.com/photos/219692/pexels-photo-219692.jpeg" alt="" />
+                                <button onClick={() => setQuery("City")} className='text-center-car text-white font-bold text-2xl'>City</button>
+                            </div></SwiperSlide>
+                            <SwiperSlide><div className='car-in-div relative max-w-xs overflow-hidden bg-cover bg-no-repeat'>
+                                <img className='max-w-xs transition duration-300 ease-in-out hover:scale-110' src="https://images.pexels.com/photos/1779487/pexels-photo-1779487.jpeg" alt="" />
+                                <button onClick={() => setQuery("Workspace")} className='text-center-car text-white font-bold text-2xl'>Workspace</button>
+                            </div></SwiperSlide>
+                            <SwiperSlide><div className='car-in-div relative max-w-xs overflow-hidden bg-cover bg-no-repeat'>
+                                <img className='max-w-xs transition duration-300 ease-in-out hover:scale-110' src='https://images.pexels.com/photos/262959/pexels-photo-262959.jpeg' alt="" />
+                                <button onClick={() => setQuery("Food")} className='text-center-car text-white font-bold text-2xl'>Food</button>
+                            </div></SwiperSlide>
+                            <SwiperSlide><div className='car-in-div relative max-w-xs overflow-hidden bg-cover bg-no-repeat'>
+                                <img className='max-w-xs transition duration-300 ease-in-out hover:scale-110' src="https://images.pexels.com/photos/58997/pexels-photo-58997.jpeg" alt="" />
+                                <button onClick={() => setQuery("Dog")} className='text-center-car text-white font-bold text-2xl'>Dog</button>
+                            </div></SwiperSlide>
+                            <SwiperSlide><div className='car-in-div relative max-w-xs overflow-hidden bg-cover bg-no-repeat'>
+                                <img className='max-w-xs transition duration-300 ease-in-out hover:scale-110' src="https://images.pexels.com/photos/417173/pexels-photo-417173.jpeg" alt="" />
+                                <button onClick={() => setQuery("Mountain")} className='text-center-car text-white font-bold text-2xl'>Mountain</button>
+                            </div></SwiperSlide>
+                            <SwiperSlide><div className='car-in-div relative max-w-xs overflow-hidden bg-cover bg-no-repeat'>
+                                <img className='max-w-xs transition duration-300 ease-in-out hover:scale-110' src="https://images.pexels.com/photos/1133957/pexels-photo-1133957.jpeg" alt="" />
+                                <button onClick={() => setQuery("Flower")} className='text-center-car text-white font-bold text-2xl'>Flower</button>
+                            </div></SwiperSlide>
+                        </Swiper>
+                    </div>
+                    {/* Top category End */}
+
+                    {loading && <Loading></Loading>}
+
+                    {/* All images section */}
+                    <div className="cate-nav navbar-start lg:flex justify-between m-auto">
                         <ul className="menu menu-horizontal px-1">
-                            <Link to="" className='rounded-md font-semibold text-lg text-gray-500 hover:text-gray-800 text-center p-3'>All</Link>
-                            <Link to="" className='navlink rounded-md text-gray-500 hover:text-gray-800 font-semibold text-lg text-center p-3 '>Photos</Link>
-                            <Link to="" className='navlink rounded-md text-gray-500 hover:text-gray-800 font-semibold text-lg text-center p-3'> Videos</Link>
-                            <Link to="" className='navlink rounded-md text-gray-500 hover:text-gray-800 font-semibold text-lg text-center p-3'> Freebies</Link>
-                            <Link to="" className='navlink rounded-md text-gray-500 hover:text-gray-800 font-semibold text-lg text-center p-3'>360</Link>
+                            <Link onClick={() => setQuery("All")} className='rounded-md font-semibold text-lg text-gray-500 hover:text-gray-800 text-center p-3'>All</Link>
+                            <Link onClick={() => setQuery("photos")} className='navlink rounded-md text-gray-500 hover:text-gray-800 font-semibold text-lg text-center p-3 '>Photos</Link>
+                            <Link onClick={() => setQuery("videos")} to="" className='navlink rounded-md text-gray-500 hover:text-gray-800 font-semibold text-lg text-center p-3'> Videos</Link>
+                            <Link onClick={() => setQuery("Freebies")} to="" className='navlink rounded-md text-gray-500 hover:text-gray-800 font-semibold text-lg text-center p-3'> Freebies</Link>
+                            <Link onClick={() => setQuery("360")} to="" className='navlink rounded-md text-gray-500 hover:text-gray-800 font-semibold text-lg text-center p-3'>360</Link>
                         </ul>
                         <div className="dropdown dropdown-bottom dropdown-end">
-                            <div tabIndex={0} role="button" className="btn m-1">Recommanded</div>
-                            <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
-                                <li><a>Most Recent</a></li>
-                                <li><a>Most Viewed</a></li>
-                                <li><a>Most Downloaded</a></li>
-                                <li><a>Most Appreciated</a></li>
+                            <div tabIndex={0} role="button" className="btn btn-sm mt-5">Recommanded</div>
+                            <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-gray-800 text-white rounded-box w-52">
+                                <li className='bg-gray-800 hover:bg-slate-600'><a>Most Recent</a></li>
+                                <li className='bg-gray-800 hover:bg-slate-600'><a>Most Viewed</a></li>
+                                <li className='bg-gray-800 hover:bg-slate-600'><a>Most Downloaded</a></li>
+                                <li className='bg-gray-800 hover:bg-slate-600'><a>Most Appreciated</a></li>
                             </ul>
                         </div>
                     </div>
@@ -108,20 +163,11 @@ const Banner = () => {
                         }
                     </div>
                     <div className="join grid grid-cols-2 join-btn">
-                        <button disabled={data?.page === 1} onClick={() => getPhotos("back")} className="join-item btn btn-outline btn-md">Previous page</button>
+                        <button disabled={data?.page === 1} onClick={() => getPhotos("back")} className="join-item btn btn-outline bg- btn-md">Previous page</button>
                         <button onClick={() => getPhotos("next")} className="join-item btn btn-outline btn-md">Next</button>
                     </div>
                 </div>
             </div>
-
-
-
-
-
-
-
-
-
         </>
 
     );
--- src/Pages/Shared/Navbar/NavBar.jsx
+++ src/Pages/Shared/Navbar/NavBar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FaFacebook, FaTwitter, FaYoutube, FaLinkedin, FaGithub, FaInstagram } from "react-icons/fa";
 import './NavBar.css';
+import logo from '../../../assets/logo.png'
 
 const NavBar = () => {
     const navOptions =
@@ -9,11 +10,13 @@ const NavBar = () => {
             <Link to="" className='rounded-md font-semibold text-gray-400 hover:text-gray-800 text-center p-3'>Explore</Link>
             <Link to="" className='navlink rounded-md text-gray-400 hover:text-gray-800 font-semibold  text-center p-3 '>Discover</Link>
             <Link to="" className='navlink rounded-md text-gray-400 hover:text-gray-800 font-semibold  text-center p-3'>For Professionals</Link>
-            <div className="dropdown text-gray-400 hover:text-gray-800">
+            <div className="dropdown dropdown-hover text-gray-400 hover:text-gray-800 hidden md:block">
                 <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path>
+                    </svg>
                 </div>
-                <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow rounded-box w-52 bg-gray-800 text-gray-300">
+                <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow rounded-box w-52 bg-gray-800 text-gray-300 hidden md:block">
                     <li className='bg-gray-800 hover:bg-gray-700'><a>About ArtAlley</a></li>
                     <li className='bg-gray-800 hover:bg-gray-700'><a>Pricing</a></li>
                     <li className='bg-gray-800 hover:bg-gray-700'><a>Licence</a></li>
@@ -23,7 +26,7 @@ const NavBar = () => {
                     <li className='bg-gray-800 hover:bg-gray-700'><a>Join The Team</a></li>
                     <li className='border-t-2 border-gray-400'>
                         <nav>
-                            <div className="grid grid-flow-col gap-4 py-2"> 
+                            <div className="grid grid-flow-col gap-4 py-2">
                                 <a><FaTwitter className='social text-amber-600'></FaTwitter></a>
                                 <a><FaYoutube className='social text-amber-600'></FaYoutube></a>
                                 <a><FaFacebook className='social text-amber-600'></FaFacebook></a>
@@ -53,7 +56,7 @@ const NavBar = () => {
                         {navOptions}
                     </ul>
                 </div>
-                <a href='/' className="btn btn-ghost text-2xl text-amber-600 font-bold ms-4">ArtAlley</a>
+                <a href='/' className="btn btn-ghost text-2xl text-amber-600 font-bold ms-4"><img className='logo' src={logo} alt="" /> ArtAlley</a>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
