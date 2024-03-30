d62b32c420267832dbed3ae6938deaa428637030
Rater7tara <nowshinakteremu005@gmail.com>
2024-03-30 22:01:36 +0600

    color change

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -6,10 +6,8 @@ import { Link } from 'react-router-dom';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination } from 'swiper/modules';
 
-
 import 'swiper/css';
 import 'swiper/css/pagination';
-
 
 const Banner = () => {
     const [query, setQuery] = useState("beautiful");
